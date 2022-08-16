fe093db94ceffe14c182780dc1711336860c8c5e
EldarOpera <109095435+EldarOpera@users.noreply.github.com>
2022-08-16 11:40:16 +0300

    Add files via upload

---
+++ script/game.js
@@ -0,0 +1,33 @@
+const generateRandomNumber = () => {
+  return Math.round(Math.random() * 10);
+};
+  
+const sum = (num1, num2) => {
+  return num1 + num2;
+};
+  
+const startRound = () => {
+  const randomNumber1 = generateRandomNumber();
+  const randomNumber2 = generateRandomNumber();
+  const answer = prompt(`Определите сумму чисел ${randomNumber1} и ${randomNumber2}. Введите ваш ответ:`);
+  const correctAnswer = sum(randomNumber1, randomNumber2);
+    
+  if (answer === `${correctAnswer}`) {
+    alert("Верно!");
+    return true;
+  } else {
+    alert(`Неверно! Правильный ответ: ${correctAnswer}`);
+    return false;
+  }
+};
+  
+const startGame = () => {
+    for (let i = 0; i < 3; i += 1) {
+      const isCorrect = startRound();
+      if (!isCorrect) {
+        alert("В следующий раз используй калькулятор :)");
+        return;
+      }
+    }
+    alert("Молодец, уважаешь математику!");
+};
