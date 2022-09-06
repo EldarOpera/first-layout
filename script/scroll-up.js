1983f133c66d828282f2fd1e63097c631ce01db5
EldarOpera <109095435+EldarOpera@users.noreply.github.com>
2022-09-06 17:35:01 +0300

    Delete scroll-up.js

--- script/scroll-up.js
+++
@@ -1,9 +0,0 @@
-let t;
-function up() {
-	let top = Math.max(document.body.scrollTop,document.documentElement.scrollTop);
-	if(top > 0) {
-		window.scrollBy(0,-100);
-		t = setTimeout('up()',15);
-	} else clearTimeout(t);
-	return false;
-};
